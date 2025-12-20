5f25942583d57822b510bf0da34e31daae931da5
XxX9Void9XxX <brooksm@carbonschools.org>
2025-12-19 21:01:36 -0700

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,30 +1,29 @@
 const socket = io();
-const canvas = document.getElementById("game");
-const ctx = canvas.getContext("2d");
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-let state = { players: {}, bullets: [] };
-let keys = { w: false, a: false, s: false, d: false };
+let keys = { w:false, a:false, s:false, d:false };
 let mouseAngle = 0;
+let state = { players:{}, bullets:[] };
+
+socket.emit("init", {
+  name: playerName.value,
+  color: playerColor.value
+});
 
 document.addEventListener("keydown", e => {
-  if (keys[e.key] !== undefined) keys[e.key] = true;
+  if (keys[e.key]) keys[e.key] = true;
 });
-
 document.addEventListener("keyup", e => {
-  if (keys[e.key] !== undefined) keys[e.key] = false;
+  if (keys[e.key]) keys[e.key] = false;
 });
 
-document.addEventListener("mousemove", e => {
+canvas.addEventListener("mousemove", e => {
   const dx = e.clientX - canvas.width / 2;
   const dy = e.clientY - canvas.height / 2;
   mouseAngle = Math.atan2(dy, dx);
   socket.emit("aim", mouseAngle);
 });
 
-document.addEventListener("mousedown", () => {
+canvas.addEventListener("mousedown", () => {
   socket.emit("shoot");
 });
 
@@ -32,40 +31,4 @@ setInterval(() => {
   socket.emit("keys", keys);
 }, 1000 / 60);
 
-socket.on("state", serverState => {
-  state = serverState;
-});
-
-function drawTank(x, y, angle, isYou) {
-  ctx.save();
-  ctx.translate(x, y);
-  ctx.rotate(angle);
-
-  ctx.fillStyle = isYou ? "#4af" : "#aaa";
-  ctx.beginPath();
-  ctx.arc(0, 0, 15, 0, Math.PI * 2);
-  ctx.fill();
-
-  ctx.fillRect(0, -4, 28, 8);
-  ctx.restore();
-}
-
-function loop() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  for (const id in state.players) {
-    const p = state.players[id];
-    drawTank(p.x, p.y, p.angle, id === socket.id);
-  }
-
-  ctx.fillStyle = "#ff0";
-  for (const b of state.bullets) {
-    ctx.beginPath();
-    ctx.arc(b.x, b.y, 4, 0, Math.PI * 2);
-    ctx.fill();
-  }
-
-  requestAnimationFrame(loop);
-}
-
-loop();
+socket.on("state", s => state = s);
