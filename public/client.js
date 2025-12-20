770040226aadd797ab0407043d29416b4807c9e8
XxX9Void9XxX <brooksm@carbonschools.org>
2025-12-19 20:55:16 -0700

    Update client.js

--- public/client.js
+++ public/client.js
@@ -5,46 +5,64 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let players = {};
+let state = { players: {}, bullets: [] };
+let keys = { w: false, a: false, s: false, d: false };
 let mouseAngle = 0;
+
+document.addEventListener("keydown", e => {
+  if (keys[e.key] !== undefined) keys[e.key] = true;
+});
+
+document.addEventListener("keyup", e => {
+  if (keys[e.key] !== undefined) keys[e.key] = false;
+});
 
 document.addEventListener("mousemove", e => {
   const dx = e.clientX - canvas.width / 2;
   const dy = e.clientY - canvas.height / 2;
   mouseAngle = Math.atan2(dy, dx);
+  socket.emit("aim", mouseAngle);
+});
+
+document.addEventListener("mousedown", () => {
+  socket.emit("shoot");
 });
 
 setInterval(() => {
-  socket.emit("move", { angle: mouseAngle });
+  socket.emit("keys", keys);
 }, 1000 / 60);
 
-socket.on("state", serverPlayers => {
-  players = serverPlayers;
+socket.on("state", serverState => {
+  state = serverState;
 });
 
-function drawTank(x, y, angle) {
+function drawTank(x, y, angle, isYou) {
   ctx.save();
   ctx.translate(x, y);
   ctx.rotate(angle);
 
-  // body
-  ctx.fillStyle = "#4af";
+  ctx.fillStyle = isYou ? "#4af" : "#aaa";
   ctx.beginPath();
   ctx.arc(0, 0, 15, 0, Math.PI * 2);
   ctx.fill();
 
-  // barrel
-  ctx.fillRect(0, -4, 25, 8);
-
+  ctx.fillRect(0, -4, 28, 8);
   ctx.restore();
 }
 
 function loop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  for (const id in players) {
-    const p = players[id];
-    drawTank(p.x, p.y, p.angle);
+  for (const id in state.players) {
+    const p = state.players[id];
+    drawTank(p.x, p.y, p.angle, id === socket.id);
+  }
+
+  ctx.fillStyle = "#ff0";
+  for (const b of state.bullets) {
+    ctx.beginPath();
+    ctx.arc(b.x, b.y, 4, 0, Math.PI * 2);
+    ctx.fill();
   }
 
   requestAnimationFrame(loop);
